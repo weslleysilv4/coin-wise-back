c2fb6e75955c20052a6ff80cae2f44f43bd1ac60
Victor Gabriel <victor_tiss@outlook.com>
2025-02-07 21:35:55 -0300

    feat(auth): change login messages & add refreshToken to controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,48 +1,55 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
-const { PrismaClient } = require('@prisma/client')
 const logger = require('../logger')
-const { userValidation } = require('../validations/user.validation')
-
-const userClient = new PrismaClient().user
+const AuthService = require('../services/auth.service')
 
 const authController = {
   async login(req, res) {
     try {
-      await userValidation.parse(req.body)
       const { email, password } = req.body
+      const { accessToken, refreshToken, user } = await AuthService.login(
+        email,
+        password
+      )
 
-      const user = await userClient.findUnique({
-        where: {
-          email,
-        },
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'Strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
       })
 
-      if (!user) {
-        return res.status(400).json({ message: 'User not found' })
-      }
-
-      if (!(await bcrypt.compare(password, user.password))) {
-        logger.warn(`Failed login attempt for user: ${user.email}`)
-        return res.status(400).json({ message: 'Invalid Credentials' })
-      }
-
-      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-        expiresIn: '1d',
+      return res.json({
+        sucess: true,
+        message: 'Sucessfully logged in!',
+        accessToken,
+        user,
       })
-
-      logger.info(`User logged in: ${user.name}`)
-      res.status(200).json({ message: 'Login successful', token: token })
     } catch (error) {
-      // Handle Zod validation errors
       if (error?.name === 'ZodError') {
-        const message = error.issues[0]?.message || 'Validation error'
-        return res.status(400).json({ message })
+        return res.status(401).json({
+          success: false,
+          code: 'VALIDATION_ERROR',
+          message:
+            'There are errors in your request. Please correct the highlighted fields.',
+          errors: {
+            email:
+              error.issues.find((item) => item.path[0] === 'email')?.message ||
+              null,
+            password:
+              error.issues.find((item) => item.path[0] === 'password')
+                ?.message || null,
+          },
+        })
       }
-      logger.error(`Login error: ${error.message}`)
-      res.status(400).json({ message: error || 'An error occurred' })
+      return res.status(401).json({
+        sucess: false,
+        code: error.code,
+        message: error.message,
+        errors: error.errors,
+      })
     }
   },
+
   authenticateToken(req, res, next) {
     const token = req.header('Authorization')?.split(' ')[1]
 
@@ -61,6 +68,17 @@ const authController = {
       req.user = user
       next()
     })
+  },
+
+  async refreshToken(req, res) {
+    try {
+      const oldRefreshToken = req.cookies.refreshToken
+      const newAcessToken = await AuthService.refreshToken(oldRefreshToken)
+
+      return res.json({ sucess: true, accessToken: newAcessToken })
+    } catch (error) {
+      return res.status(401).json({ sucess: false, message: error.message })
+    }
   },
 }
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,7 @@
 const router = require('express').Router()
 const authController = require('../controllers/auth.controller')
 
-router.post('/login', (req, res) => {
-  return authController.login(req, res)
-})
+router.post('/login', authController.login)
+router.post('/auth/refresh', authController.refreshToken)
 
 module.exports = router
