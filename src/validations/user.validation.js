22234a8b97d101cfb4bca3d7b541be5ef7826747
Victor Gabriel <victor_tiss@outlook.com>
2025-02-21 22:06:15 -0300

    feat(user): improve user validation with trim & custom messages

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -1,11 +1,18 @@
 const z = require('zod')
 
 const userValidation = z.object({
-  email: z.string().email(),
+  email: z
+    .string()
+    .trim()
+    .toLowerCase()
+    .email({ message: 'Invalid email address' }),
   password: z
     .string()
-    .min(6, { message: 'Password must be 5 or more characters long' })
-    .max(255),
+    .trim()
+    .min(6, { message: 'Password must be 6 or more characters long' })
+    .max(255, {
+      message: 'Password must be less than or equal to 255 characters',
+    }),
 })
 
 module.exports = userValidation
